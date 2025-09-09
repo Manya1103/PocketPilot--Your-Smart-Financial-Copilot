f1908e526c4249065671b438f992151e789c4095
Manya <manya111203@gmail.com>
2025-09-09 10:38:20 +0530

    mongoDB setup

--- PP-backend/index.js
+++ PP-backend/index.js
@@ -1,23 +1,25 @@
 import express from "express";
+import dotenv from "dotenv";
 import cors from "cors";
+import connectDB from './config/mongoDB.js';
+
+dotenv.config();
+
+connectDB();
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
-app.use(cors({
-  origin: "http://localhost:5173", // frontend URL
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
-}));
+app.use(cors({ origin: "http://localhost:5173" }));
 
 // Routes
 app.get("/", (req, res) => {
-  res.send("Hello from backend!");
+    res.send("Hello from backend!");
 });
 
 // Start server
 app.listen(PORT, () => {
-  console.log(`🚀 Server running at http://localhost:${PORT}`);
+    console.log(`🚀 Server running at http://localhost:${PORT}`);
 });
